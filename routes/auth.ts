34ac739361b5e5a499086dc3b927391217a4b716
iamrobins <itsrobin4@gmail.com>
2023-01-26 19:19:12 +0530

    check user and save route added

---
+++ routes/auth.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+const router = express.Router();
+import User from "../models/User.js";
+import verifyUser from "../middlewares/verifyUser.js";
+
+interface CustomReq extends Request {
+  firebaseId: string;
+}
+
+const authUser = async (req: any, res: Response) => {
+  try {
+    const firebaseId = req.firebaseId;
+    console.log(firebaseId);
+
+    if (await User.findOne({ firebaseId })) {
+      return res.status(200).json({ success: "User already registered" });
+    }
+    await User.create({
+      firebaseId: firebaseId,
+    });
+
+    return res
+      .status(201)
+      .json({ success: "New user registered successfully" });
+  } catch (error: any) {
+    return res.status(401).json(error.message);
+  }
+};
+
+router.route("/").get(verifyUser, authUser);
+
+export default router;
