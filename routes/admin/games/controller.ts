26423e539e9ac940feb7ccd56176e007dd8c1a5b
iamrobins <itsrobin4@gmail.com>
2023-01-27 21:49:31 +0530

    added post and get game apis for admins

--- routes/admin/games/controller.ts
+++ routes/admin/games/controller.ts
@@ -1,8 +1,26 @@
-import e, { Response } from "express";
+import { Response } from "express";
 import Game from "../../../models/Game";
 
 export async function getGames(req: any, res: Response) {
-  return res.json({ success: true, data: "access allowed" });
+  try {
+    const games = await Game.find({});
+    if (!games) throw Error("Games not found");
+    res.status(200).json({ success: true, data: games });
+  } catch (e) {
+    return res.status(400).json({ success: false, data: e });
+  }
+}
+
+export async function getGame(req: any, res: Response) {
+  const slug = req.params.slug;
+
+  try {
+    const game = await Game.findOne({ slug });
+    if (!game) throw Error("game not found");
+    return res.json({ success: true, data: game });
+  } catch (e) {
+    return res.status(400).json({ success: false, data: e });
+  }
 }
 
 export async function postGame(req: any, res: Response) {
@@ -10,8 +28,18 @@ export async function postGame(req: any, res: Response) {
 
   try {
     gameBody.firebaseId = req.firebaseId;
-    const game = await Game.create(gameBody);
-    return res.json({ success: true, data: game });
+    await Game.create(gameBody);
+    return res.json({ success: true });
+  } catch (e) {
+    return res.status(400).json({ success: false, data: e });
+  }
+}
+
+export async function putGame(req: any, res: Response) {
+  const gameBody = req.body;
+  try {
+    await Game.updateOne({ _id: gameBody.id }, gameBody);
+    return res.json({ success: true });
   } catch (e) {
     return res.status(400).json({ success: false, data: e });
   }
--- routes/admin/games/index.ts
+++ routes/admin/games/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import verifyAdmin from "../../../middlewares/verifyAdmin";
-import { getGames, postGame } from "./controller";
+import { getGame, getGames, postGame, putGame } from "./controller";
 const router = express.Router();
 
 // GET ROUTES
+router.route("/:slug").get(verifyAdmin, getGame);
 router.route("/").get(verifyAdmin, getGames);
 router.route("/").post(verifyAdmin, postGame);
+router.route("/").put(verifyAdmin, putGame);
 
 export default router;
